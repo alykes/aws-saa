56408e9b17bf1d210bc9aa066af0874d69f3cbcf
alykes <anthony.gouskos@gmail.com>
2024-03-21 18:00:13 +1100

    Adding some route53 and health check notes

--- code/ec2-user-data.sh
+++ code/ec2-user-data.sh
@@ -6,4 +6,5 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo “<h1>Yiasou from $(hostname -f)</h1>” > /var/www/html/index.html
+EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo “<h1> Yiasou from $(hostname -f) located in $EC2_AZ </h1>” > /var/www/html/index.html
